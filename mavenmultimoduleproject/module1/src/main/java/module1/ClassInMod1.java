2afe18cf68de30ee34c02c82532e75deda5c8090
dattatray <dattatray111@gmail.com>
2020-05-27 19:47:42 +0530

    for demo

---
+++ mavenmultimoduleproject/module1/src/main/java/module1/ClassInMod1.java
@@ -0,0 +1,8 @@
+package module1;
+
+public class ClassInMod1 
+{
+	public static void main(String[] args) {
+		System.out.println("m1c1");
+	} 
+}
---
+++ mavenmultimoduleproject/module2/src/main/java/module2/ClassInModule2.java
@@ -0,0 +1,9 @@
+package module2;
+
+public class ClassInModule2 {
+	
+	public static void main(String[] args) {
+		System.out.println("class in m2");
+	}
+
+}
